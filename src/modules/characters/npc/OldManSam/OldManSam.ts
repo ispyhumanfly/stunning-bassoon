a490f5077fa934b648644b0e2f517aa5a360759e
ispyhumanfly <ispyhumanfly@gmail.com>
2022-04-09 21:05:05 -0400

    Checking in new wandering merchant characger class. Updated Sally aspreite data.

--- src/modules/characters/npc/OldManSam/OldManSam.ts
+++ src/modules/characters/npc/OldManSam/OldManSam.ts
@@ -5,8 +5,8 @@ class OldManSam extends Actor {
     constructor() {
         super({
             pos: vec(150, 150),
-            width: 100,
-            height: 100,
+            width: 200,
+            height: 200,
         });
     }
 
--- src/modules/characters/npc/WanderingMerchant/WanderingMerchant.ts
+++ src/modules/characters/npc/WanderingMerchant/WanderingMerchant.ts
@@ -2,19 +2,7 @@ import { Actor, Die, vec } from "excalibur";
 import { Resources } from "../../../../resources";
 
 class WanderingMerchant extends Actor {
-    constructor() {
-        super({
-            pos: vec(100, 100),
-            width: 100,
-            height: 100,
-        });
-    }
-
-    onInitialize() {
-        this.graphics.add(Resources.Sword.toSprite());
-        this.actions.blink(500, 190);
-        this.vel.x = 8;
-    }
+    public items = 100;
 }
 
 export default WanderingMerchant;
