9684124741b6df7b22d915cfa358930e563058ee
ispyhumanfly <ispyhumanfly@gmail.com>
2022-04-10 17:03:51 -0400

    Anemo goblin type.

---
+++ src/modules/characters/npc/Goblin/Anemo/Anemo.ts
@@ -0,0 +1,33 @@
+import { AsepriteResource } from "@excaliburjs/plugin-aseprite";
+import { Actor, Die, ImageSource, vec } from "excalibur";
+import Goblin from "../Goblin";
+import AnemoImage from "./Horus.png";
+
+class Anemo extends Goblin {
+    constructor() {
+        super({
+            pos: vec(245, 245),
+            width: 100,
+            height: 100,
+        });
+    }
+
+    onInitialize() {
+        this.graphics.add(Resources.Image.toSprite());
+
+        this.actions
+            .delay(5000)
+            .repeatForever((ctx) => ctx.moveBy(200, 0, 20).moveBy(-200, 0, 20));
+    }
+}
+
+const Resources = {
+    Image: new ImageSource(AnemoImage),
+    AsepriteResource: new AsepriteResource(
+        "./modules/characters/npc/Goblin/Anemo/Anemo.json"
+    ),
+};
+
+export { Resources };
+
+export default Anemo;
